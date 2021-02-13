ca23e884c7b892b9ed9216845c25143dcb9229c0
Mihai Balaniscu <mihaiba@amazon.com>
2021-02-13 21:41:41 +0200

    add failing test for required use cases for FizzBuzz

--- src/test/java/com/fizz/buzz/FizzBuzzTest.java
+++ src/test/java/com/fizz/buzz/FizzBuzzTest.java
@@ -2,21 +2,34 @@ package com.fizz.buzz;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 public class FizzBuzzTest {
 
     private FizzBuzz sut;
 
     @BeforeEach
-    public void init(){
+    public void init() {
         sut = new FizzBuzz();
     }
 
-    @Test
-    public void testConvertValue1() {
-        String expected = "1";
-        String actual = sut.map(1);
+    @ParameterizedTest
+    @MethodSource("generateTestInput")
+    public void testConvertValue(int input, String expected) {
+        String actual = sut.map(input);
         Assertions.assertEquals(expected, actual);
+    }
+
+    private static Stream<Arguments> generateTestInput() {
+        return Stream.of(
+                Arguments.of(1, "1"),
+                Arguments.of(3, "fizz"),
+                Arguments.of(5, "buzz"),
+                Arguments.of(15, "fizzbuzz")
+        );
     }
 }
