faf86bbbad8854910f4ebc396b5279b65a17dc8e
Mihai Balaniscu <mihaiba@amazon.com>
2021-02-13 21:52:57 +0200

    implement requirements Step 1

--- src/main/java/com/fizz/buzz/FizzBuzz.java
+++ src/main/java/com/fizz/buzz/FizzBuzz.java
@@ -1,7 +1,26 @@
 package com.fizz.buzz;
 
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 public class FizzBuzz {
-    public String map(int value) {
-        return String.valueOf(value);
+    private static final String FIZZ = "fizz";
+    private static final String BUZZ = "buzz";
+
+    public String map(int input) {
+        String collector = "";
+        if (input % 3 == 0) {
+            collector = FIZZ;
+        }
+        if (input % 5 == 0) {
+            collector += BUZZ;
+        }
+        return collector.isEmpty() ? String.valueOf(input) : collector;
+    }
+
+    public static void main(String[] args) {
+        FizzBuzz service = new FizzBuzz();
+        String result = IntStream.rangeClosed(1, 20).mapToObj(service::map).collect(Collectors.joining(" "));
+        System.out.println(result);
     }
 }
