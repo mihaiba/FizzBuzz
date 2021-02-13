019d5619f243e3a5f8f5afdbdad1a6173a2f0965
Mihai Balaniscu <mihaiba@amazon.com>
2021-02-13 23:29:07 +0200

    add rest application properties and entry point

---
+++ src/test/java/com/fizz/buzz/RestServiceApplicationTests.java
@@ -0,0 +1,12 @@
+package com.fizz.buzz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class RestServiceApplicationTests {
+    
+    @Test
+    public void contextLoads() {
+    }
+}
