518b7015d5b2b87cf07e7e3c2d61f037b5576460
Mihai Balaniscu <mihaiba@amazon.com>
2021-02-14 01:41:20 +0200

    register GlobalExceptionHandler

--- src/main/java/com/fizz/buzz/GlobalExceptionHandler.java
+++ src/main/java/com/fizz/buzz/GlobalExceptionHandler.java
@@ -5,6 +5,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
@@ -19,6 +20,7 @@ import java.util.Map;
 
 import static java.util.stream.Collectors.toList;
 
+@ControllerAdvice
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(ConstraintViolationException.class)
